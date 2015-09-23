aa69a682fdd73a7a5114cb3e990afffd9ae6b4ba
Cole Ogden <coleogden@Coles-MacBook-Pro.local>
2015-09-23 16:17:20 -0500

    init commit

---
+++ src/motorturn.java
@@ -0,0 +1,12 @@
+/**
+ * Created by coleogden on 9/23/15.
+ */
+import rxtxrobot.*;
+
+public class motorturn {
+    public static void main(String[] args) {
+        System.out.println("swag");
+    }
+
+}
+
